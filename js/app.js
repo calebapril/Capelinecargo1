6c588907f77543400e608e9a298e4f65c0dbb625
calebapril <calebessien001@gmail.com>
2022-09-17 16:59:17 +0100

    project

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    // Hero Slider
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items: 1,
+        dots: false,
+        smartSpeed: 1000,
+        navText: ['Prev', 'Next'],
+        responsive:{
+            0:{
+                
+            },
+            600:{
+                
+            },
+            1000:{
+                
+            }
+        }
+    })
+})
